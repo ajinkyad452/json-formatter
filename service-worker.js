8cd6e6f7b53b9df2a35c346b9086f14836144abb
Ajinkya Dube <dube.ajinkya452@gmail.com>
2024-10-12 00:51:36 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'json-formatter-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'json-formatter.umd.min.js',
+  'custom.css',  // Add your CSS file here
+  'custom.js',
+  'favicon128.png',
+  'favicon.png'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
